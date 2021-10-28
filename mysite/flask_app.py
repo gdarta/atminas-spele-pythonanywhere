e7263e3b9d2ed03970abe9627bd8612351db9e35
gdarta <dartagobleja@gmail.com>
2021-10-28 08:31:00 +0000

    pievienoju failus vnk

--- mysite/flask_app.py
+++ mysite/flask_app.py
@@ -1,15 +1,31 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-#def hello_world():
-   #return 'Hello from my stinky butt!'
+
 @app.route('/')
-def huina():
+def sakums():
     return render_template('main_page.html')
+
+@app.route('/rezultats', methods = ["GET","POST"])
+def rezultats():
+    if request.method == "POST":
+        pirmais = request.form['pirmais']
+        otrais = request.form['otrais']
+        rez = int(pirmais)*int(otrais)
+        return render_template('main_page.html', rez = int(rez))
+
+@app.route('/spelite')
+def laukums():
+    return render_template('atminas_spele.html')
+
+if __name__ == '__main__':
+    app.run()
+
+
 
 
